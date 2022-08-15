460f7fde287e2b24d0604179ea77887c204d18df
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-08-15 03:19:21 -0400

    Changes in navbar and theme button styles and HomePage with initial design ready

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,20 @@
+import Img from '../assets/perfil_1.png';
 
 export const HomePage = () => {
     return (
-        <div className="container">
-            <h1>HomePage</h1>
+        <div className="home__container">
+            <div className="home__container--title">
+               <h2>Hello World!</h2>
+               <h1>I'm <span>Ivan Panussis</span></h1>
+            </div>
+            <div className="home__container--image">
+                <img src={ Img } alt='Profile image from the home page.'/>
+            </div>
+            <div className="home__container--description">
+                <p> I'm a profesional passionate about </p>
+                <p> development of web and mobile </p>
+                <p> applications. </p>
+            </div>
         </div>
     )
 }
