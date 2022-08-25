9ed7e13407e07ead70c5c18f8401626a9bdff2fd
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-08-25 00:18:53 -0400

    HomePage mobile version finished

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
+
 import Img from '../assets/perfil_1.png';
+import Pdf from '../assets/CV.pdf';
 
 export const HomePage = () => {
     return (
@@ -14,6 +17,8 @@ export const HomePage = () => {
                 <p> I'm a profesional passionate about </p>
                 <p> development of web and mobile </p>
                 <p> applications. </p>
+                <br />
+                <Link to={ Pdf } target='_blank' download><i className="fa-solid fa-file"></i>Download CV</Link>
             </div>
         </div>
     )
